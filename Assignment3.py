e930e56e9d32a8df5b83d0669ee55dd43b9fe2ff
isak lundgren <isak.leopold@gmail.com>
2023-09-22 16:31:40 +0200

    Pre home

--- Assignment3.py
+++ Assignment3.py
@@ -1,4 +1,5 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
 
 # Define degree trigonometric funtions
@@ -32,12 +33,70 @@ def profileLoss(s_l, alpha_in, alpha_out):
 
 # Known quantities
 phi_set = 0.7156
-phi = np.linspace(0.5, 1, 100)
-psi = 0.5894
-DF = 0.45
-Reaction = 0.5
-T01 = 300  # K
-P01 = 101325  # Pa
-rho = 1.225  # kg m-3
-gamma = 1.4
-R = 287
+phi_list = np.linspace(0.5, 1, 100)
+
+
+def totalToTotalEff(phi):
+    psi = 0.5894
+    DF = 0.45
+    R = 0.5
+
+    # Calculate angles from R, psi, phi
+    alpha1 = alpha3 = arctand((1 / phi) * (R + psi/2 - 1))  # Eq 4.14
+    alpha2 = arctand(2 / phi * (1-R) + tand(alpha1))  # Eq 4.13a
+    beta2 = arctand(tand(alpha2) - 1 / phi)  # Eq 4.15
+    beta1 = arctand(2*R/phi - tand(beta2))
+
+    # Calculate pitch-chord ratios
+    s_l_rotor = (DF + cosd(alpha1) / cosd(alpha2) - 1) / (1 / 2 * (tand(alpha1) - tand(alpha2)) * cosd(alpha1))
+    s_l_stator = (DF + cosd(alpha2) / cosd(alpha3) - 1) / (1 / 2 * (tand(alpha2) - tand(alpha3)) * cosd(alpha2))
+
+    # Bugfixing
+    c_x = 10
+    c_1 = c_x / cosd(alpha1)
+    c_2 = c_x / cosd(alpha2)
+    c_3 = c_x / cosd(alpha3)
+    c_t1 = c_x * tand(alpha1)
+    c_t2 = c_x * tand(alpha2)
+    c_t3 = c_x * tand(alpha3)
+
+    DF_recalc_rotor = (1 - c_2 / c_1) + (c_t1 - c_t2) / (2 * c_1) * s_l_rotor
+    DF_recalc_stator = (1 - c_3 / c_2) + (c_t2 - c_t3) / (2 * c_2) * s_l_stator
+
+    # Fetch profile loss coefficients
+    Y_p_rotor = profileLoss(s_l_rotor, alpha1, alpha2)
+    Y_p_stator = profileLoss(s_l_stator, alpha2, alpha3)
+
+    # Construct total-to-total efficiency
+    result = 1 - 1/2 * phi ** 2 / psi * (Y_p_rotor / (cosd(beta1) ** 2) + Y_p_stator / (cosd(alpha2) ** 2))
+    return result, s_l_stator, s_l_rotor
+
+
+# Return answers
+eta_tt_set, dump1, dump2 = totalToTotalEff(phi_set)
+eta_tt, s_l_statorList, s_l_rotorList = totalToTotalEff(phi_list)
+
+# Print set answer
+print(f'η_tt: {eta_tt_set:.3g}.')
+
+# Plot continous efficiency
+fig1, ax1 = plt.subplots()
+ax1.plot(phi_list, eta_tt * 100, '-r')
+ax1.set_title('Total-to-total efficiency for different flow coefficients')
+ax1.set_ylabel('η_tt [%]')
+ax1.set_xlabel('Φ [-]')
+ax1.grid()
+
+# Plot continous rotor/stator pitch-chord ratios
+fig2, ax2 = plt.subplots()
+ax2.plot(phi_list, s_l_statorList, '-r', label="stator")
+ax2.plot(phi_list, s_l_rotorList, '-b', label="rotor")
+ax2.set_title('Pitch-chord ratios for different flow coefficients')
+ax2.set_ylabel('s/l')
+ax2.set_xlabel('Φ [-]')
+ax2.grid()
+ax2.legend()
+
+# Show plots
+plt.show()
+
